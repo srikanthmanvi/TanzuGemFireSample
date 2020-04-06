f21c4223ef269e441802de726dca1a60670ec46b
smanvi <smanvi@pivotal.io>
2020-04-06 16:57:28 -0400

    Add property to application.properties for TLS

--- README.md
+++ README.md
@@ -10,5 +10,4 @@ Provide the name of the service instance that has been created in [manifest.yml]
 
 ### To bind to a TLS enabled service instance
 In addition to providing the name of the service instance in the manifest as mentioned in the above section, 
-you need to activate the spring profile which is used in this sample app to configure client side tls.
-You can activate the spring profile by using [SPRING_PROFILES_ACTIVE in the manifest.yml](manifest.yml#L8). In this sample the name of the spring profile which which turns on TLS is `tls`. Hence to connect to a TLS service instance update the manifest  `SPRING_PROFILES_ACTIVE: tls`, if you are connecting to a non-tls service instance don't set any value for `SPRING_PROFILES_ACTIVE`.
+you add `spring.data.gemfire.security.ssl.use-default-context=true` to [application.properties](src/main/resources/application.properties).
--- src/main/java/io/pivotal/cloudcache/sample/PccSampleAppPizzaStoreApplication.java
+++ src/main/java/io/pivotal/cloudcache/sample/PccSampleAppPizzaStoreApplication.java
@@ -2,10 +2,7 @@ package io.pivotal.cloudcache.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
-import org.springframework.data.gemfire.config.annotation.EnableSsl;
 import org.springframework.geode.config.annotation.EnableClusterAware;
 
 @SpringBootApplication
@@ -16,11 +13,4 @@ public class PccSampleAppPizzaStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PccSampleAppPizzaStoreApplication.class, args);
 	}
-
-	@Profile("tls")
-	@EnableSsl(useDefaultContext = true)
-	class TLSConfig{}
 }
-
-
-//@ClientCacheApplication(servers = @ClientCacheApplication.Server(host="localhost", port = 40404))
